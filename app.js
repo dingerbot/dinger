c7e8c202dd2e661f406c2d9d1ea2ab120a50d5fb
Hong <hongj77@gmail.com>
2014-06-21 19:41:00 -0500

    chatbox

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 function dingerController($scope) {
 
+	$scope.questions = [];
+	$scope.answers = ['fuck you'];
+	$scope.answer = '';
+
 	$scope.hitEnter = function () {
-		$scope.ding = "\"fuck u\"";
-		document.getElementById('text-input').blur();
+		if ($scope.answer) {
+			var p = document.createElement('p');
+			var node = document.createTextNode('You: ' + $scope.answer);
+			p.appendChild(node);
+			var element = document.getElementById('chat');
+			element.appendChild(p);
+
+			$scope.answer = '';
+
+		}
 	};
+
+
 
 
 
