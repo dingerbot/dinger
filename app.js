dc10bf2cefd8fee8f9d19ea26bff704860334afa
Hong <hongj77@gmail.com>
2014-06-21 19:55:16 -0500

    additions

--- app.js
+++ app.js
@@ -6,13 +6,32 @@ function dingerController($scope) {
 
 	$scope.hitEnter = function () {
 		if ($scope.answer) {
+			document.getElementById('text-input').disabled = true;
 			var p = document.createElement('p');
 			var node = document.createTextNode('You: ' + $scope.answer);
 			p.appendChild(node);
 			var element = document.getElementById('chat');
 			element.appendChild(p);
 
+			var p2 = document.createElement('p');
+			var node2 = document.createTextNode('George: ' + 'fuck you');
+			p2.appendChild(node2);
+			var element2 = document.getElementById('chat');
+
+
+			
+
+			setTimeout(function() {
+				element2.appendChild(p2);
+				document.getElementById('text-input').disabled = false;
+				document.getElementById('text-input').focus();
+				
+			}, 1000);
+
+		
+
 			$scope.answer = '';
+
 
 		}
 	};
