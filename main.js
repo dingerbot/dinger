e4345d76e0ff9239cedc424cc82c4131edea2344
Hong <hongj77@gmail.com>
2014-06-23 23:47:45 -0500

    added sounds, comments

--- main.js
+++ main.js
@@ -3,14 +3,17 @@ function dingerController($scope, $timeout) {
 	$scope.hitEnter = function () {
 		if ($scope.answer) {
 			
+			// underscore templating feature
 			var userTemplate = _.template( $('#user-template').html() );
 			$('.chat-padding-bottom').before( userTemplate({query: $scope.answer}) );
+			// update scrolling 60% gloves ATK
 			document.getElementById('chat-box').scrollTop = document.getElementById('chat-box').scrollHeight;
 
 			$scope.answer = '';
 			$('#text-input').attr('placeholder', '');
 			document.getElementById('text-input').disabled = true;
 
+			// version 1 intelligence. exact matching | random from list
 			var possRespon = juiced[$scope.answer];
 			if (!possRespon || possRespon.length == 0 || possRespon[0].length == 0) {
 				possRespon = juiced[_.sample(Object.keys(juiced))];
@@ -24,12 +27,16 @@ function dingerController($scope, $timeout) {
 		var responseToPost = Math.floor(Math.random() * possRespon.length);
 		$scope.possRespon = possRespon;
 
+		// say everything in key list
 		for (var i = 0; i < $scope.possRespon[responseToPost].length; i++) {
 			interval += randomizer($scope.possRespon[responseToPost][i]);
 
 			$timeout(function() {
 				var georgeTemplate = _.template($('#george-template').html());
 				$('.chat-padding-bottom').before( georgeTemplate( {response: $scope.possRespon[responseToPost].shift()} ) );
+
+				var fbsound = new Audio('fb.mp3');
+				fbsound.play();
 
 				document.getElementById('chat-box').scrollTop = document.getElementById('chat-box').scrollHeight;
 
@@ -38,6 +45,7 @@ function dingerController($scope, $timeout) {
 					document.getElementById('text-input').disabled = false;
 					document.getElementById('text-input').focus();
 				}
+
 			}, interval);
 		}
 	}
